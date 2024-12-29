63d9c67deb397697f3ca32790695bfa0f68aeb08
shimjaedoo <woen1637@naver.com>
2024-12-29 16:00:25 +0900

    [Feat] 상품 상세조회 추가

---
+++ src/components/product/_core/product.api.ts
@@ -0,0 +1,20 @@
+import { apiInstance } from "../../../_common/axios-api.instance.ts";
+import { IProductModel, IProductPublicFindListResponse, IProductPublicFindResponse } from "../_interface/product.interface.ts";
+
+export const productApi = {
+  gets: async function (page: string) {
+    const { data } = await apiInstance.get<IProductPublicFindListResponse<IProductModel[]>>("/products", {
+      params: {
+        limit: "10",
+        page,
+        sort: "CREATED_AT:DESC",
+      },
+    });
+    return data;
+  },
+
+  get: async function (id: number) {
+    const { data } = await apiInstance.get<IProductPublicFindResponse<IProductModel>>(`/products/${id}`);
+    return data;
+  },
+};
