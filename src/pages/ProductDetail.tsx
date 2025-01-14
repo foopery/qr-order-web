2dede9a64c8a897ba4bbcf0378deedbbdcfceecf
shimjaedoo <woen1637@naver.com>
2025-01-14 19:39:29 +0900

    [Chore] 경로 변경

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,8 +1,8 @@
-import ProductDetailView from "./views/ProductDetailView.tsx";
+import ProductDetailView from "../components/product/detail/views/ProductDetailView.tsx";
 import { useParams } from "react-router-dom";
-import { productQuery } from "../_core/product.query.ts";
-import ProductDetailErrorView from "./views/ProductDetailErrorView.tsx";
-import ProductDetailLoadingView from "./views/ProductDetailLoadingView.tsx";
+import { productQuery } from "../components/product/_core/product.query.ts";
+import ProductDetailErrorView from "../components/product/detail/views/ProductDetailErrorView.tsx";
+import ProductDetailLoadingView from "../components/product/detail/views/ProductDetailLoadingView.tsx";
 
 export default function ProductDetail() {
   const param = useParams();
