e21ca6fd88d91c3ea70dd705a1710646c2a45a09
shimjaedoo <woen1637@naver.com>
2025-01-14 19:38:18 +0900

    [Feat] 장바구니 페이지 Router 추가

--- Router.tsx
+++ Router.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Product from "./src/components/product/gets/Product";
+import Product from "./src/pages/Product.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProductDetail from "./src/components/product/detail/ProductDetail.tsx";
+import ProductDetail from "./src/pages/ProductDetail.tsx";
+import Cart from "./src/pages/Cart.tsx";
 
 export default function Router() {
   const routers: { path: string; element: React.ReactNode; key: string }[] = [
@@ -14,6 +15,11 @@ export default function Router() {
       path: "/product/:id", // 상품 상세페이지
       element: <ProductDetail />,
       key: "product/detail/:id",
+    },
+    {
+      path: "/cart", // 장바구니 페이지
+      element: <Cart />,
+      key: "cart",
     },
   ];
   return (
